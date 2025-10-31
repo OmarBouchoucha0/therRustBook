1023440a447546f4159d001b5f2ca8a8e441f09e
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2025-10-31 19:45:05 +0100

    completed the guessing game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,26 @@
+use std::{cmp::Ordering, io};
+
+use rand::Rng;
+
+fn main() {
+    loop {
+        println!("Guess The Number! Between 1 and 10");
+        let mut guess = String::new();
+        let target: u32 = rand::thread_rng().gen_range(1..=10);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed To Read Line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        match guess.cmp(&target) {
+            Ordering::Less => println!("Too Small"),
+            Ordering::Greater => println!("Too Big"),
+            Ordering::Equal => {
+                println!("YOU GUEESED CORRECT !!");
+                break;
+            }
+        }
+    }
+}
