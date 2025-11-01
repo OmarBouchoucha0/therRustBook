2ed31e9192357959bbc6604c6ecca596556655dd
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2025-11-01 14:12:39 +0100

    functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,7 +1,24 @@
-fn function(x: i32) -> i32 {
-    x + 5
+use std::io;
+
+fn fibonacci(x: i32) -> i32 {
+    if x == 0 {
+        0
+    } else if x == 1 {
+        1
+    } else {
+        fibonacci(x - 1) + fibonacci(x - 2)
+    }
 }
 fn main() {
-    let y = function(5);
-    println!("the value of y = {y}");
+    let mut intput = String::new();
+    println!("PUT NUMBER");
+    io::stdin()
+        .read_line(&mut intput)
+        .expect("ERROR READING LINES");
+    let number: i32 = intput.trim().parse().expect("ENTER A NUMBER");
+    println!(
+        "the fibonacci sequence of {} : {}",
+        number,
+        fibonacci(number),
+    );
 }
