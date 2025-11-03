2d4f1de531cc81b01cb90860b6739855edbe2690
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2025-11-03 11:37:32 +0100

    updated

---
+++ enums/src/main.rs
@@ -0,0 +1,18 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+fn value_in_cents(coin: &Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+fn main() {
+    let coin = Coin::Penny;
+    println!("the value of the coin : {}", value_in_cents(&coin));
+}
