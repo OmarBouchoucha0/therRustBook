c4878fa55015ab551fb9adc5275539708c4cd4a1
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2025-11-25 19:50:03 +0100

    a

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -1,13 +1,22 @@
 use std::sync::mpsc::{self, RecvError};
 use std::thread;
+use std::time::Duration;
 
 fn main() -> Result<(), RecvError> {
     let (tx, rx) = mpsc::channel();
     let _handle = thread::spawn(move || {
-        let val = String::from("Hello");
-        tx.send(val).unwrap();
+        let vals = [
+            String::from("Hello"),
+            String::from("Hello"),
+            String::from("Hello"),
+        ];
+        for val in vals {
+            tx.send(val).unwrap();
+            std::thread::sleep(Duration::from_secs(1));
+        }
     });
-    let value = rx.recv()?;
-    println!("{:?}", value);
+    for recived in rx {
+        println!("recived : {:?}", recived);
+    }
     Ok(())
 }
